e7d9a20b3116119971222d731541979935081be1
Sunday Lee <isundaylee.reg@gmail.com>
2012-10-18 11:30:03 -0400

    Initial Commit

---
+++ VOCABI/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  VOCABI
+//
+//  Created by Jiahao Li on 10/18/12.
+//  Copyright (c) 2012 Jiahao Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
